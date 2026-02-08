08d14095b59d8323174829512d84d4218dc39910
Nielsniz-SVG <nielsboulengernincq@gmail.com>
2026-02-08 18:35:48 +0100

    Add database initialization and file management functions

--- src/database/storage.js
+++ src/database/storage.js
@@ -1,84 +1,49 @@
+// src/database/storage.js
+
+/**
+ * Initializes the database and manages file storage operations.
+ */
+
 const fs = require('fs');
 const path = require('path');
-const crypto = require('crypto');
 
-const DATA_DIR = path.join(__dirname, '../..', 'data');
+/**
+ * Initializes the database by creating necessary directories or files.
+ * @param {string} dbPath - Path to the database.
+ */
+function initializeDatabase(dbPath) {
+    if (!fs.existsSync(dbPath)) {
+        fs.mkdirSync(dbPath, { recursive: true });
+        console.log(`Database initialized at ${dbPath}`);
+    } else {
+        console.log(`Database already exists at ${dbPath}`);
+    }
+}
 
-// Ensure data directory exists
-const ensureDataDirectory = () => {
-  if (!fs.existsSync(DATA_DIR)) {
-    fs.mkdirSync(DATA_DIR, { recursive: true });
-  }
-};
+/**
+ * Saves data to a file in the database.
+ * @param {string} fileName - The name of the file to save data.
+ * @param {string} data - The data to be saved.
+ * @param {string} dbPath - Path to the database.
+ */
+function saveToFile(fileName, data, dbPath) {
+    fs.writeFileSync(path.join(dbPath, fileName), data, 'utf8');
+    console.log(`Data saved to ${fileName}`);
+}
 
-// File paths
-const getFilePath = (filename) => path.join(DATA_DIR, `${filename}.json`);
+/**
+ * Reads data from a file in the database.
+ * @param {string} fileName - The name of the file to read data from.
+ * @param {string} dbPath - Path to the database.
+ * @returns {string} - The data read from the file.
+ */
+function readFromFile(fileName, dbPath) {
+    const filePath = path.join(dbPath, fileName);
+    if (fs.existsSync(filePath)) {
+        return fs.readFileSync(filePath, 'utf8');
+    } else {
+        throw new Error(`File ${fileName} does not exist.`);
+    }
+}
 
-// Initialize file if it doesn't exist
-const initializeFile = (filename, defaultData = []) => {
-  const filePath = getFilePath(filename);
-  if (!fs.existsSync(filePath)) {
-    fs.writeFileSync(filePath, JSON.stringify(defaultData, null, 2));
-  }
-};
-
-// Read data from file
-const readData = (filename) => {
-  initializeFile(filename);
-  const filePath = getFilePath(filename);
-  const data = fs.readFileSync(filePath, 'utf-8');
-  return JSON.parse(data);
-};
-
-// Write data to file
-const writeData = (filename, data) => {
-  ensureDataDirectory();
-  const filePath = getFilePath(filename);
-  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-};
-
-// Add data to file
-const addData = (filename, newData) => {
-  const data = readData(filename);
-  data.push(newData);
-  writeData(filename, data);
-};
-
-// Update data in file
-const updateData = (filename, id, updatedData) => {
-  const data = readData(filename);
-  const index = data.findIndex(item => item.id === id);
-  if (index !== -1) {
-    data[index] = { ...data[index], ...updatedData };
-    writeData(filename, data);
-    return true;
-  }
-  return false;
-};
-
-// Find data in file
-const findData = (filename, condition) => {
-  const data = readData(filename);
-  return data.find(condition);
-};
-
-// Filter data in file
-const filterData = (filename, condition) => {
-  const data = readData(filename);
-  return data.filter(condition);
-};
-
-// Generate unique ID
-const generateId = () => crypto.randomBytes(16).toString('hex');
-
-module.exports = {
-  ensureDataDirectory,
-  readData,
-  writeData,
-  addData,
-  updateData,
-  findData,
-  filterData,
-  generateId,
-  getFilePath
-};
+module.exports = { initializeDatabase, saveToFile, readFromFile };
